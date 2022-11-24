588d39b5315b634a6fce9e00cdbf0237ee8bcae7
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2022-11-23 23:10:14 -0600

    agrego CRUD en articlesDAO

--- server/dao/articlesDao.js
+++ server/dao/articlesDao.js
@@ -1,9 +1,23 @@
 const articles = require("../models/articles")
-
 class ArticlesDAO {
   async getAll() {
     const articlesDB = await articles.find({})
     return articlesDB
+  }
+
+  async getById(id) {
+    const article = await articles.findById(id)
+    return article
+  }
+
+  async update({ _id, name, body }) {
+    await articles.updateOne({ _id }, { $set: name, body })
+    return { message: "Article successfully updated" }
+  }
+
+  async deleteById(id) {
+    await articles.deleteOne({ _id })
+    return { message: "Article deleted" }
   }
 }
 
