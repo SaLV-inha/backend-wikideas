94c397c4b9955d2c3a5d221b42eea07b7d8a6f4c
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2022-12-14 09:59:40 -0600

    agrego _id al modelo

--- server/models/articles.js
+++ server/models/articles.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const collection = "articles"
 
 const schema = new mongoose.Schema({
+  _id: { type: String },
   name: { type: String, require: true, max: 50 },
   body: { type: String, require: true },
 })
